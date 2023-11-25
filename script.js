25d3c45c198d5d310cddbc8f4e011af308195a5b
ArjunB126 <arjunbajare86@gmail.com>
2023-11-25 23:13:22 +0530

    New_Commit

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const languageDropdown = document.getElementById("language-btn")
 const faqListItem = document.querySelectorAll(".inner-container ul li")
 
 languageDropdown.addEventListener("click", () => {
-    
+    document.getElementById("language-dropdown").classList.toggle("show")
   })
